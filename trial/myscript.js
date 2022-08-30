77b951105de0407b9789b1b6c05fe8c32d3fa540
AsmaAlhajeri <103028094+AsmaAlhajeri@users.noreply.github.com>
2022-08-30 12:02:23 +0400

    Piece of cake
    Piece of cake

---
+++ trial/myscript.js
@@ -0,0 +1,32 @@
+function makecake() {
+        
+    document.getElementById("strawberrycake").innerHTML =`<image src = "decocake.gif"  width="800" height="650" style="margin-left:500px; margin-top:30px">`
+    var element = document.getElementById ("button1");
+    element.remove();
+}
+
+
+function finallook() {
+        
+    document.getElementById("strawberrycake").innerHTML =`<image src = "cakefinal.gif"  width="800" height="650" style="margin-left:500px; margin-top:30px">`
+    var element = document.getElementById ("button2");
+    element.remove();
+}
+
+function recipe() {
+        
+    document.getElementById("strawberrycake").innerHTML =`<image src = "cakestraw.png"  width="800" height="750" style="margin-left:500px; margin-top:30px">`
+    var element = document.getElementById ("button3");
+    element.remove();
+}
+
+function slice() {
+        
+    document.getElementById("strawberrycake").innerHTML =`<image src = "cakepink.gif"  width="800" height="650" style="margin-left:500px; margin-top:30px">`
+    // var element = document.getElementById ("button4");
+    // element.remove();
+}
+
+
+
+
